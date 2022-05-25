0ea4012909e40129a5c1ba174b2b8aa27b51bce5
S. Roland Marshall <samuel.r.marshall@gmail.com>
2022-05-24 22:33:31 -0400

    convert to haml

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Adds Puma-dev to hosts
+  config.hosts << "wedding.test"
+
 end
