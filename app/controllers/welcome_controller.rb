fdb3903e41437db2974c3d125b94d67f5c45190e
S. Roland Marshall <samuel.r.marshall@gmail.com>
2022-05-28 19:37:00 -0400

    whole buncha shit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
-  def index; end
+  def index
+    # Time in 12 hr est
+    @ceremony_time =
+      Time
+        .parse(Rails.application.credentials.CEREMONY_TIME)
+        .strftime('%l:%M %p')
+  end
 
   def miller
     num = rand(1..7)
